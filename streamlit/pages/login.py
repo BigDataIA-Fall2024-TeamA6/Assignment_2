74048c77f9f226dd0be93c8ea28b898ad102ace3
vishodhan-krishnan <krishnan.vis@northeastern.edu>
2024-10-05 22:40:15 -0400

    Integrated FastAPI for login

--- streamlit/pages/login.py
+++ streamlit/pages/login.py
@@ -3,8 +3,11 @@ import bcrypt
 from pages.db import DBConnection
 from fastapi import FastAPI
 import mysql.connector
+import requests
 
 app = FastAPI()
+FASTAPI_URL = "http://127.0.0.1:8000"
+
 
 def create_connection():
     """Create a database connection."""
@@ -28,36 +31,25 @@ def main():
     password = st.text_input("password",type="password",key='pword')
     user_type = st.selectbox("Choose User type",options=['user','admin'],key='select_user_type')
 
-        
-    if st.button("Log in",key='login'):
-        is_valid, user_type_db = verify_user(username, password)
-        if is_valid and user_type_db == user_type:
+    if st.button("Login",key='login'):
+
+        response = requests.post(f"{FASTAPI_URL}/token", json={"username":username,"password": password,"user_type":user_type})
+        if response.status_code == 200:
+            st.session_state.username = username
             if user_type == 'user':
-                st.success(f"Welcome {username}!")
                 st.switch_page("pages/user_landing.py")
             elif user_type == 'admin':
-                st.success(f"Welcome Admin!")
                 st.switch_page("pages/admin.py")
+        elif response.status_code == 500:
+            st.error(f"Error {response.status_code} Server down :(")
         else:
-            st.error("Invalid credentials!")
+            st.error(f"Error {response.status_code} Please check credentials or user type!")
+        
                 
 
     if st.button("Create User",key='create_user'):
         st.switch_page("pages/create_user.py") 
 
-
-def verify_user(uname,pword):
-    db = create_connection()
-    cursor = db.cursor()
-    cursor.execute("SELECT password, user_type FROM login WHERE username = %s", (uname,))
-    result = cursor.fetchone()
-    cursor.close()
-    db.close()
-    if result:
-        hashed_password = result[0].encode('utf-8')
-        if bcrypt.checkpw(pword.encode('utf-8'), hashed_password):
-            return True, result[1]  # Return user type
-    return False, None  # Invalid credentials
 
 if __name__ == "__main__":
     main()
